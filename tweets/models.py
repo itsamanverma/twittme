48cf147817034ebb661e5fd08cc88485e42d7e81
itsamanverma <amanvermame786@gmail.com>
2020-09-25 23:12:04 +0530

    dynamic url

--- tweets/models.py
+++ tweets/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+
 class Tweet(models.Model):
     # id = models.AutoFeild(primary_key=True)
     content = models.TextField(max_length=200)
-    image   = models.FileField(upload_to='images/', blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
 
+from .models import Tweet
 # Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """ Rest api view
+        consume by javascript or Swift/java/Ios/Andriod 
+        return json data """
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except:
+        raise Http404
+
+        data = {
+            "id" : tweet_id,
+            "content": obj.content,
+            #'image_path': obj.image.url
+        }
+    return JsonResponse(data) # json.dump content_type ='application/json'
