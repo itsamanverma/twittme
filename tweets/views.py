057303fe0b336f4b9d5926127e94f0f56c10851b
itsamanverma <amanvermame786@gmail.com>
2020-10-04 23:49:36 +0530

    dynamic html template

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 
@@ -15,7 +16,7 @@ def tweet_list_view(request, *args, **kwargs):
         return json data
     """
     qs = Tweet.objects.all()
-    tweet_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweet_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 100)} for x in qs]
     data = {
         "isUser": False,
         "response": tweet_list,
